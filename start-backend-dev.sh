89c8cb79b65fd4b6680beec5ae8f5118be75b3f8
lancel00zz <patrick.zakher@datadoghq.com>
2025-06-02 10:48:10 +0200

    change

--- start-backend-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get local IP address for QR routing
-export HOST_IP=$(ipconfig getifaddr en0)
-
-# Start backend container using prebuilt image
-docker-compose -f docker-compose.dev.yml down
-docker compose pull
-docker-compose -f docker-compose.dev.yml up
