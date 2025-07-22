5f714a489028cbd1a847adf120712136c96db5dd
lancel00zz <patrick.zakher@datadoghq.com>
2025-07-22 18:49:12 +0200

    Configure Your App to Use the Database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
 
     from .routes import main
     app.register_blueprint(main)
