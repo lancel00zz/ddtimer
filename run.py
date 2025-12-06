e1de02d4fa1b4763434c7ccdf55ea487fb14c8b4
lancel00zz <patrick.zakher@datadoghq.com>
2025-12-06 20:16:48 +0100

    Add Python logging configuration for trace-log correlation
    - Configure Python logging with console handler
    - Enable Werkzeug to use Python logging
    - Allows ddtrace DD_LOGS_INJECTION to inject trace_id and span_id
    - Logs will now correlate with traces in Datadog

--- run.py
+++ run.py
@@ -2,9 +2,40 @@
 from ddtrace import patch_all
 patch_all()
 
+import logging
+from logging.config import dictConfig
+
+# Configure Python logging for trace correlation
+# This enables ddtrace to inject trace_id and span_id into logs
+dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+            'stream': 'ext://sys.stdout'
+        }
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console']
+    }
+})
+
 from app import create_app
 
 app = create_app()
+
+# Configure Flask/Werkzeug to use Python logging
+import logging as werkzeug_logging
+werkzeug_logger = werkzeug_logging.getLogger('werkzeug')
+werkzeug_logger.setLevel(logging.INFO)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5050)
